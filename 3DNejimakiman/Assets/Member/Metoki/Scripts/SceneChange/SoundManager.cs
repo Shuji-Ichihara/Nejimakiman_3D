26a728055998f78a6081d2a736dd9a2e5bb17685
masatometoki <21c1020049mm@edu.tech.ac.jp>
2023-08-03 14:41:57 +0900

    タイトルシーンとチュートリアル、一つ目プレイシーン遷移までやってみた

---
+++ 3DNejimakiman/Assets/Member/Metoki/Scripts/SceneChange/SoundManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource bgmAudioSource;
+    [SerializeField] AudioSource seAudioSource;
+
+    [SerializeField] List<BGMSoundData> bgmSoundDatas;
+    [SerializeField] List<SESoundData> seSoundDatas;
+
+    public float MasterVolume = 1;
+    public float BgmMasterVolume = 1;
+    public float SeMasterVolume = 1;
+
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(BGMSoundData.BGM bgm)
+    {
+        BGMSoundData data = bgmSoundDatas.Find(data => data.bgm == bgm);
+        bgmAudioSource.clip = data.audioClip;
+        bgmAudioSource.volume = data.volume * BgmMasterVolume * MasterVolume;
+        bgmAudioSource.Play();
+    }
+
+
+    public void PlaySE(SESoundData.SE se)
+    {
+        SESoundData data = seSoundDatas.Find(data => data.se == se);
+        seAudioSource.volume = data.volume * SeMasterVolume * MasterVolume;
+        seAudioSource.PlayOneShot(data.audioClip);
+    }
+
+}
+
+[System.Serializable]
+public class BGMSoundData
+{
+    public enum BGM
+    {
+        Title,
+        Dungeon,
+        Hoge,
+    }
+
+    public BGM bgm;
+    public AudioClip audioClip;
+    [Range(0, 1)]
+    public float volume = 1;
+}
+
+[System.Serializable]
+public class SESoundData
+{
+    public enum SE
+    {
+        Start,
+        Goal,
+        Attack,
+        Damage,
+        Hoge,
+    }
+
+    public SE se;
+    public AudioClip audioClip;
+    [Range(0, 1)]
+    public float volume = 1;
+}
